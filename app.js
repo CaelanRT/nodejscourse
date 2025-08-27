60e70c73738bffda640b1c53682e260af4dd45e9
Caelan Roberge-Toll <caelanrt15@gmail.com>
2025-08-26 21:04:16 -0400

    added some dependencies

--- app.js
+++ app.js
@@ -6,3 +6,22 @@
 
 // global dependency - use it in any project
 // npm install -g <packageName>
+
+// package.json - manifest file (stores important info about projec/package)
+
+//3 ways to make this
+// manual approach (create package.json in the root, create properties etc)
+
+// automate
+// npm init (step by step)
+// npm init -y (everything default setup)
+// why do you need? when you have a package.json it will add a dependencies object to that file when you install a package
+// package.json is VERY IMPORTANT
+
+
+const _ = require('lodash');
+
+// returns this array of arrays as a flat array
+const items = [1, [2,[3,[4]]]];
+const newItems = _.flattenDeep(items);
+console.log(newItems);
