975b553d94df9de9a68236f63e942741814c6136
Caelan Roberge-Toll <caelanrt15@gmail.com>
2025-08-26 21:18:24 -0400

    making more dependency changes

--- app.js
+++ app.js
@@ -17,7 +17,13 @@
 // npm init -y (everything default setup)
 // why do you need? when you have a package.json it will add a dependencies object to that file when you install a package
 // package.json is VERY IMPORTANT
+// the package.json will let you share the code and then install the dependencies locally if you do npm install once you download the code
 
+// dev dependencies you can install using -D
+// dev dependencies are for dev things and not necesarily for production, it will help you restart your application (for nodenom specifically)
+
+// scripts in package.json is a json object that lets you set up local commands to run
+// use npm run <command name> to run it
 
 const _ = require('lodash');
 
@@ -25,3 +31,5 @@ const _ = require('lodash');
 const items = [1, [2,[3,[4]]]];
 const newItems = _.flattenDeep(items);
 console.log(newItems);
+console.log('hello people');
+
