2e13e32ba479ef471b7175cc1e15f390abbadae7
Caelan Roberge-Toll <caelanrt15@gmail.com>
2025-08-28 21:51:38 -0400

    finally fun express things!

--- master_files/02-express-tutorial/app.js
+++ master_files/02-express-tutorial/app.js
@@ -1,39 +1,28 @@
-// starting express, servers on easymode!
-// require express you need to install express
 const express = require('express');
-
+// requiring path module
+const path = require('path');
 const app = express();
 
-// need to add the resource, and a callback, which again has the 2 args of req and res
-// everytime the user hits rood with a get, you get this 
-app.get('/', (req, res) => {
-    res.status(200).send('Home Page');
+// app.use is what you're using in the response, and all your static resources are in the folder by this name, usually public
+// app.use is for setting up middleware
+app.use(express.static('./public'))
+
+// now everything from your static resources is now servable!!!
+
+app.get('/', (req, res)=>{
+    // this needs the absolute path so you're resolving the dirname then getting to the directory you want
+    res.sendFile(path.resolve(__dirname, './navbar-app/index.html'));
 })
 
-app.get('/about', (req, res) => {
-    res.status(200).send('About Page');
+app.all('*', (req, res)=>{
+    res.status(404).send('resource not found')
 })
-
-app.all('*', (req, res) => {
-    res.status(404).send('<h1>resource not found</h1>')
-})
-
-// important methods
-// app.get - request
-// app.post - request
-// app.put - request
-// app.delete - request
-// app.all - handles all http verbs
-// app.use
-// app.listen
 
 
 
 
 app.listen(5000, ()=>{
     console.log("server is listening on port 5000");
-    
 })
-
 
 
