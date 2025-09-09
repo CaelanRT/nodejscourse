5e37ac3079b57d760cd53398bdf9308906f0db76
Caelan Roberge-Toll <caelanrt15@gmail.com>
2025-09-08 21:35:36 -0400

    figuring out route parameters finally, this is cool!

--- master_files/02-express-tutorial/app.js
+++ master_files/02-express-tutorial/app.js
@@ -21,6 +21,31 @@ app.get('/api/products', (req, res)=> {
     res.json(newProducts);
 })
 
+// // item 1 route
+// app.get('/api/products/1', (req, res)=> {
+//     // getting a single product using the find method
+//     const singleProduct = products.find((product)=> product.id === 1)
+
+//     res.json(singleProduct);
+// })
+
+// this is it with a route parameter
+app.get('/api/products/:productID', (req, res)=> {
+    
+    console.log(req.params);
+    // you can destructure the params and the access the value that is passed
+    const {productID} = req.params;
+
+    const singleProduct = products.find((product) => product.id === Number(productID));
+
+    if (!singleProduct) {
+        return res.status(404).send('Product does not exist');
+    }    
+
+    return res.json(singleProduct);
+    
+})
+
 // listen to the server
 app.listen(5000, ()=>{
     console.log('Server is listening on port 5000...');
