7aa0f329fbd80a18643dfd9c92203c4228689d0c
Caelan Roberge-Toll <caelanrt15@gmail.com>
2025-09-08 21:25:00 -0400

    map function and params

---
+++ master_files/02-express-tutorial/myCode/6-ssrvsapi.js
@@ -0,0 +1,29 @@
+const express = require('express');
+// requiring path module
+const path = require('path');
+const app = express();
+
+// app.use is what you're using in the response, and all your static resources are in the folder by this name, usually public
+// app.use is for setting up middleware
+app.use(express.static('./public'))
+
+// now everything from your static resources is now servable!!!
+
+// app.get('/', (req, res)=>{
+//     // this needs the absolute path so you're resolving the dirname then getting to the directory you want
+//     res.sendFile(path.resolve(__dirname, './navbar-app/index.html'));
+// })
+
+//  there are 2 options for sending your files, static assests and serverside rendering
+// index.html is the root now and will always get server correctly
+
+app.all('*', (req, res)=>{
+    res.status(404).send('resource not found')
+})
+
+
+app.listen(5000, ()=>{
+    console.log("server is listening on port 5000");
+})
+
+
