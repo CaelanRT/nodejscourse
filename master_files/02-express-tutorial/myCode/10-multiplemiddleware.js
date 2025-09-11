c0a91a78f793a82eba6d62433a224a831dc8ffa3
Caelan Roberge-Toll <caelanrt15@gmail.com>
2025-09-11 16:35:53 -0400

    stuff

--- master_files/02-express-tutorial/myCode/10-multiplemiddleware.js
+++ master_files/02-express-tutorial/myCode/10-multiplemiddleware.js
@@ -2,13 +2,22 @@ const express = require('express');
 const app = express();
 const logger = require('./logger');
 const authorize = require('./authorize');
+const morgan = require('morgan');
 
 // this will invoke logger for any route
+// will we use middleware in routes? yes. add middleware to routes you want those functions to run on
+// can add multiple middleware to routes using the array thing
+// middleware options - our own / express' own middleware / 3rd party
 // you need to have this above your requests
 // if you give it a path as a param, the middleware will only apply after that path is hit
 //app.use('/api',logger);
 
-app.use([logger, authorize]);
+// app.use([logger, authorize]);
+
+// app.use has a method called static and its looking for the public folder, puts all contents of public folder as static assets
+// app.use(express.static('./public'))
+app.use(morgan('tiny'))
+
 
 app.get('/', (req, res)=>{
     res.send('Home');
