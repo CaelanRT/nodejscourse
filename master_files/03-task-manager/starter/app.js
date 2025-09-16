c98bcb73acbb846599b7b1063677537244c2d37c
Caelan Roberge-Toll <caelanrt15@gmail.com>
2025-09-16 17:49:35 -0400

    adding routes and controllers

--- master_files/03-task-manager/starter/app.js
+++ master_files/03-task-manager/starter/app.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const app = express();
+const tasks = require('./routes/tasks')
+
+// middleware
+app.use(express.json());
 
 // routes
 app.get('/hello', (req, res)=>{
     res.send('Task Manager App');
 })
+
+app.use('/api/v1/tasks', tasks)
+
+
+
+// route of '/api/v1' api routes convention is because your root is probably an html page and then you want to have some versioning
 
 const port = 3000;
 
