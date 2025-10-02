6c7d1decfe020f6af901508eedf9320e126b17a0
Caelan Roberge-Toll <caelanrt15@gmail.com>
2025-10-01 21:48:28 -0400

    finally back on the wagon

--- master_files/03-task-manager/starter/app.js
+++ master_files/03-task-manager/starter/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
-const tasks = require('./routes/tasks')
+const tasks = require('./routes/tasks');
+const connectDB = require('./db/connect');
+require('dotenv').config();
+
 
 // middleware
 app.use(express.json());
@@ -13,11 +16,21 @@ app.get('/hello', (req, res)=>{
 // route of '/api/v1' api routes convention is because your root is probably an html page and then you want to have some versioning
 app.use('/api/v1/tasks', tasks)
 
-
-
-
 const port = 3000;
 
-app.listen(port, ()=>{
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI);
+        app.listen(port, ()=>{
     console.log(`Server is listening on port ${port}...`);
 })
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
+
+start();
+
+
+
