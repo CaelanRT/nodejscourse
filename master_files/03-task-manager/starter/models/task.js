2b8676b606e7c9d5c6403bb447978d4078d354f7
Caelan Roberge-Toll <caelanrt15@gmail.com>
2025-10-02 16:53:34 -0400

    adding data validation for database schema

--- master_files/03-task-manager/starter/models/task.js
+++ master_files/03-task-manager/starter/models/task.js
@@ -1,12 +1,24 @@
 const mongoose = require('mongoose');
 
+
 // this is where you're setting up your database schema so that it only allows additions with the structure of the documents that you want
 const TaskSchema = new mongoose.Schema({
-    name:String,completed:Boolean
+    // Schema validation - need to be able to not accept empty inputs
+    name:{
+        type:String,
+        required:[true,'must provide a name'],
+        trim:true,
+        maxlength:[20, 'name cannot be more than 20 characters']
+    },
+    completed:{
+        type:Boolean,
+        default:false
+    }
 });
 
 // once you have the schema, you want to set up the model (representation for the collection)
 // once you have a model, all the tasks you add to the database then you are adding things to the collection
 // the instance of the model is called a document, model is a wrapper for a schema
+// .create() creates an instance of the model so the document and writes it to the database
 
 module.exports = mongoose.model('Task', TaskSchema);
