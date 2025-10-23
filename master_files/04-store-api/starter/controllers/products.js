d30b69ed1cbc22ae52643f49b4b9c1267119a164
Caelan Roberge-Toll <caelanrt15@gmail.com>
2025-10-23 15:52:30 -0400

    adding numeric filters and such - pretty cool stuff but honestly very simple to implement

--- master_files/04-store-api/starter/controllers/products.js
+++ master_files/04-store-api/starter/controllers/products.js
@@ -2,17 +2,15 @@ const Product = require('../models/product');
 
 const getAllProductsStatic = async (req, res) => {
 
-    const products = await Product.find({})
-    .sort('name')
-    .select('name price')
-    .limit(10)
-    .skip(1);
+    const products = await Product.find({price:{$gte:30}})
+    .sort('price')
+    .select('name price');
 
     res.status(200).json({products, nbHits: products.length});
 }
 
 const getAllProducts = async (req, res) => {
-    const { featured, company, name, sort, fields } = req.query;
+    const { featured, company, name, sort, fields, numericFilters } = req.query;
 
     // empty object, but you're passing in things into the query params and then just checking if they exist in the query
     const queryObject = {}
@@ -29,9 +27,42 @@ const getAllProducts = async (req, res) => {
         queryObject.name = {$regex:name, $options:'i'};
     }
 
+    // regex to convert the human readable filters to the mongoose ones
+    // NUMERIC FILTERING - INTERESTING LOGIC
+    if (numericFilters) {
+
+        const operatorMap = {
+            '>':'$gt',
+            '>=':'$gte',
+            '=':'$eq',
+            '<':'$lt',
+            '<=':'$lte',
+        }
+
+        const regEx = /\b(<|>|>=|<=|=)\b/g;
+        let filters = numericFilters.replace(regEx, (match)=>
+            `-${operatorMap[match]}-`
+        )
+
+        const options = ['price', 'rating'];
+        filters = filters.split(',').forEach((item)=>{
+            const [field, operator, value] = item.split('-');
+
+            if(options.includes(field)) {
+                queryObject[field] = {[operator]:Number(value)};
+            }
+        })
+
+        // console.log(filters);
+        
+    }
+
     // you are removing await here because you are no longer trying to get the promise from the find, you are chaining the sort onto the object if it exists so you want to convert it to a promise after that
     // this right away returns the documents
     // sort
+
+    console.log(queryObject);
+    
     let result = Product.find(queryObject);
     
     if (sort) {
@@ -50,6 +81,11 @@ const getAllProducts = async (req, res) => {
         result = result.select(fieldsList);
     }
 
+    // pagination
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+    const skip = (page -1) * limit;
+    result = result.skip(skip).limit(limit);
 
     const products = await result;
 
