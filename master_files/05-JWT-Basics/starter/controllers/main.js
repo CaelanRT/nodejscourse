b0171185e6063ccffbe76cf0061c4e55bcbc60e6
Caelan Roberge-Toll <caelanrt15@gmail.com>
2025-10-23 16:45:07 -0400

    JWTgit add .

---
+++ master_files/05-JWT-Basics/starter/controllers/main.js
@@ -0,0 +1,30 @@
+// check username, password in post(login) request
+// if exist, create new JWT - if not, send error
+// send back to front-end
+
+// setup authentication so only the request with JWT can access the dashboard
+
+const CustomAPIError = require('../errors/custom-error');
+
+const login = async (req, res) => {
+    const {username, password} = req.body;
+    
+    if (!username || !password) {
+        throw new CustomAPIError('please provide email and password', 400);
+    }
+
+    console.log(username, password);
+    
+    
+    res.send('Fake Login/Register/Signup')
+}
+
+const dashboard = async (req, res) => {
+    const luckyNumber = Math.floor(Math.random() * 100);
+
+    res.status(200).json({msg:`Hello, John Doe`, secret:`Here is your authorized data, your lucky number is ${luckyNumber}`});
+}
+
+module.exports = {
+    login, dashboard
+}
