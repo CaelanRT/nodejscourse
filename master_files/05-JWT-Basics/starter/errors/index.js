53c5434bd9815b6091bc331913dbe85b9aed9615
Caelan Roberge-Toll <caelanrt15@gmail.com>
2025-11-29 11:17:44 -0500

    updating custom error classes

---
+++ master_files/05-JWT-Basics/starter/errors/index.js
@@ -0,0 +1,8 @@
+const CustomAPIError = require('./custom-error')
+const BadRequest = require('./bad-requests')
+const UnauthenticatedError = require('./unauthenticated')
+
+module.exports = {
+    CustomAPIError,BadRequest,UnauthenticatedError
+}
+
--- master_files/05-JWT-Basics/starter/routes/main.js
+++ master_files/05-JWT-Basics/starter/routes/main.js
@@ -3,7 +3,11 @@ const router = express.Router();
 
 const {login, dashboard} = require('../controllers/main');
 
-router.route('/dashboard').get(dashboard);
+const authMiddleware = require ('../middleware/auth')
+
+// you add the middleware auth which has next() called that then passes to the dashboard endpoint
+//the dashboard endpoint is protected while the login one is not
+router.route('/dashboard').get(authMiddleware,dashboard);
 router.route('/login').post(login);
 
 module.exports = router;
