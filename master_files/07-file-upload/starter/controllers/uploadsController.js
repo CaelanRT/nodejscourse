0c942f50008dd340f3ca0bd2fef7062d69006d99
Caelan Roberge-Toll <caelanrt15@gmail.com>
2026-01-16 16:10:20 -0500

    finishing this file upload project, concepts make sense. skipped the cloudinary part because i wasn't interested its just an api and id rather learn how to do that using supabase/aws

--- master_files/07-file-upload/starter/controllers/uploadsController.js
+++ master_files/07-file-upload/starter/controllers/uploadsController.js
@@ -1,7 +1,31 @@
+const path = require('path');
+
 const {StatusCodes} = require('http-status-codes');
+const CustomError = require('../errors');
 
 const uploadProductImage = async (req, res) => {
-    res.send('upload product image');
+
+    if(!req.files) {
+        throw new CustomError.BadRequestError('No file uploaded.');
+    }
+
+    const productImage = req.files.image;
+
+    if(!productImage.mimetype.startsWith('image')) {
+        throw new CustomError.BadRequestError('Please upload an image.')
+    }
+
+    const maxSize = 1024 * 1024;
+
+    if(productImage.size > maxSize) {
+        throw new CustomError.BadRequestError('File too large.')
+    }
+
+    const imagePath = path.join(__dirname,'../public/uploads/' + `${productImage.name}` );
+
+    await productImage.mv(imagePath);
+
+    return res.status(StatusCodes.OK).json({image:{src:`/uploads/${productImage.name}`}});
 };
 
 
