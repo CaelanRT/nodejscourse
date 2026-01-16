e3bd42481cdef8abe6de1f91b842b556afd87e52
Caelan Roberge-Toll <caelanrt15@gmail.com>
2026-01-15 21:35:17 -0500

    finally got this project finished, added stats and were done

--- master_files/06.5-jobster-api/starter/controllers/jobs.js
+++ master_files/06.5-jobster-api/starter/controllers/jobs.js
@@ -127,17 +127,45 @@ const deleteJob = async (req, res) => {
 
 const showStats = async (req, res) => {
 
+  // this is your aggregation pipeline
   let stats = await Job.aggregate([
     {$match:{createdBy: new mongoose.Types.ObjectId(req.user.userId)}},
     {$group:{_id:'$status', count:{$sum:1}}},
   ])
 
-  console.log(stats);
+  stats = stats.reduce((acc, curr)=>{
+    const {_id:title, count} = curr;
+    acc[title] = count;
+    return acc;
+  }, {});
+
+  const defaultStats = {
+    pending: stats.pending || 0,
+    interview: stats.interview || 0,
+    declined: stats.declined || 0,
+  }
+
+  let monthlyApplications = await Job.aggregate([
+    {$match:{createdBy: new mongoose.Types.ObjectId(req.user.userId)}},
+    {$group:{
+      _id:{year:{$year:'$createdAt'},month:{$month:'$createdAt'}},
+      count:{$sum:1},
+    }},
+    {$sort:{'_id.year':-1, '_id.month': -1}},
+    {$limit:6}
+  ])
+
+  monthlyApplications = monthlyApplications.map((item) =>{
+    const {_id:{year,month}, count} = item;
+    const date = moment().month(month -1).year(year).format('MMM Y');
+    return {date, count};
+  }).reverse();
+
+  console.log(monthlyApplications);
   
 
 
-
-  res.status(StatusCodes.OK).json({defaultStats:{}, monthlyApplications:[]})
+  res.status(StatusCodes.OK).json({defaultStats, monthlyApplications})
 }
 
 module.exports = {
