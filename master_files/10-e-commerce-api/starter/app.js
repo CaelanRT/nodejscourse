c724d12ddce1d349b25c26878fa4d1c41e0a5781
Caelan Roberge-Toll <caelanrt15@gmail.com>
2026-01-17 14:13:23 -0500

    setting up the beginning of the ecommerce api project from scratch

--- master_files/10-e-commerce-api/starter/app.js
+++ master_files/10-e-commerce-api/starter/app.js
@@ -1,1 +1,47 @@
-console.log('E-Commerce API');
+// imports
+require('dotenv').config();
+require('express-async-errors');
+
+// app
+const express = require('express');
+const app = express();
+
+// middleware imports
+const notFoundMiddlware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+
+// middleware invocations
+app.use(express.json());
+app.use(notFoundMiddlware);
+app.use(errorHandlerMiddleware);
+
+
+// database
+const dbConnect = require('./db/connect');
+
+// routes
+app.get('/', (req, res) =>{
+    res.send('e-commerce api');
+});
+
+// port
+const port = process.env.PORT || 5000;
+
+// start function + connect to db
+const start = async () => {
+    try {
+        await dbConnect(process.env.MONGO_URI);
+
+        app.listen(port, () => {
+            console.log(`Server is listening on port ${port}...`);
+        });
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
+
+start();
+
+
+
