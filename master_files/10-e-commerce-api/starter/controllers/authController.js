5d5ae259bf31c4d5013add99a6ef53c37889f7e8
Caelan Roberge-Toll <caelanrt15@gmail.com>
2026-01-22 21:38:36 -0500

    setting up more auth stuff

--- master_files/10-e-commerce-api/starter/controllers/authController.js
+++ master_files/10-e-commerce-api/starter/controllers/authController.js
@@ -1,16 +1,36 @@
+const {StatusCodes} = require('http-status-codes');
+const User = require('../models/User');
+const CustomError = require('../errors');
 
 
-const register = (req, res) =>{
-    res.send('register')
+// create a user works, need to finish the rest of the vid
+const register = async (req, res) =>{
+    const {name, email, password} = req.body;
+
+    const tempEmail = await User.findOne({email});
+
+    if (tempEmail) {
+        throw new CustomError.BadRequestError('Email in use, please enter a valid email.');
+    }
+
+    // first registered user is an admin
+    const isFirstAccount = await User.countDocuments() === 0;
+    const role = isFirstAccount? 'admin':'user';
+
+    const user = await User.create({name, email, password, role});
+    
+    res.status(StatusCodes.CREATED).json(user);
 }
 
-const login = (req, res) =>{
+const login = async (req, res) =>{
     res.send('login')
 }
 
-const logout = (req, res) =>{
+const logout = async (req, res) =>{
     res.send('logout')
 }
+
+
 
 module.exports = {
     register,
