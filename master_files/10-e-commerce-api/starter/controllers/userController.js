8da3826521ef3361d4037d9dc6b8cab980bef40e
Caelan Roberge-Toll <caelanrt15@gmail.com>
2026-02-03 21:57:40 -0500

    added update password functionality

--- master_files/10-e-commerce-api/starter/controllers/userController.js
+++ master_files/10-e-commerce-api/starter/controllers/userController.js
@@ -1,6 +1,6 @@
 const {StatusCodes} = require('http-status-codes');
 const User = require('../models/User');
-const {NotFoundError} = require('../errors/index');
+const CustomError = require('../errors/index');
 
 const getAllUsers = async (req, res) => {
 
@@ -18,15 +18,16 @@ const getSingleUser = async (req, res) => {
     const user = await User.findOne({_id:req.params.id}).select('-password');
 
     if (!user) {
-        throw new NotFoundError(`No user with id ${id}`);
+        throw new CustomError.NotFoundError(`No user with id ${id}`);
     }
 
     res.status(StatusCodes.OK).json({user});
 }
 
-// show current user
+// this route just uses the authenticate user middleware and gets the req.user
+// used for when you load the page and want to check if there is an active cookie still
 const showCurrentUser = (req, res) => {
-    res.send('showcurrentuser')
+    res.status(StatusCodes.OK).json({user:req.user});
 }
 // update user
 const updateUser = (req,res) =>{
@@ -34,8 +35,32 @@ const updateUser = (req,res) =>{
 }
 
 // update user password
-const updateUserPassword = (req,res) =>{
-    res.send('updateuserpassword')
+const updateUserPassword = async (req,res) =>{
+    const {password, newPassword} = req.body;
+
+    if (!password || !newPassword) {
+        throw new CustomError.BadRequestError('Missing passwords. Please enter the old and new password.');
+    }
+
+    const id = req.user.userId;
+
+    const user = await User.findOne({_id:id});
+
+    if (!user) {
+        throw new CustomError.NotFoundError('No user found.')
+    }
+
+    const passwordMatch = await user.comparePassword(password);
+
+    if (!passwordMatch) {
+        throw new CustomError.UnauthenticatedError('Invalid password.');
+    }
+
+    user.password = newPassword;
+
+    await user.save();
+
+    res.status(StatusCodes.OK).json({user});
 }
 
 module.exports = {
--- master_files/10-e-commerce-api/starter/routes/userRoutes.js
+++ master_files/10-e-commerce-api/starter/routes/userRoutes.js
@@ -8,9 +8,9 @@ const { getAllUsers, getSingleUser, showCurrentUser, updateUser, updateUserPassw
 // placement of middleware is very important, first authenticate the user then check admin
 router.route('/').get(authenticateUser, authorizePermissions('admin'), getAllUsers);
 
-router.route('/showMe').get(showCurrentUser);
+router.route('/showMe').get(authenticateUser, showCurrentUser);
 router.route('/updateUser').patch(updateUser);
-router.route('/updateUserPassword').patch(updateUserPassword);
+router.route('/updateUserPassword').patch(authenticateUser, updateUserPassword);
 
 router.route('/:id').get(authenticateUser, getSingleUser);
 
