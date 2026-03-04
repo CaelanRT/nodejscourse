b0e3329f5136d9420014f51b3c7e3c4f36f585e9
Caelan Roberge-Toll <caelanrt15@gmail.com>
2026-03-03 21:20:41 -0500

    added the bypass for rehashing the password with .save() hook

--- master_files/10-e-commerce-api/starter/models/User.js
+++ master_files/10-e-commerce-api/starter/models/User.js
@@ -1,55 +1,52 @@
-const mongoose = require('mongoose');
-const validator = require('validator');
-const bcrypt = require('bcryptjs');
+const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
 
 const UserSchema = new mongoose.Schema({
-    name:{
-        type:String,
-        required:[true, 'Please provide name.'],
-        minlength:3,
-        maxlength: 50
+  name: {
+    type: String,
+    required: [true, "Please provide name."],
+    minlength: 3,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: [true, "Please provide email."],
+    validate: {
+      validator: validator.isEmail,
+      message: "Please provide valid email.",
     },
-    email:{
-        type:String,
-        unique:true,
-        required:[true, 'Please provide email.'],
-        validate:{
-            validator:validator.isEmail,
-            message:'Please provide valid email.',
-
-        }
-    },
-    password:{
-        type:String,
-        required:[true, 'Please provide password.'],
-        minlength:6
-    },
-    role:{
-        type:String,
-        enum:['admin', 'user'],
-        default:'user'
-    }
-})
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide password."],
+    minlength: 6,
+  },
+  role: {
+    type: String,
+    enum: ["admin", "user"],
+    default: "user",
+  },
+});
 
 // don't use an arrow function!
-UserSchema.pre('save', async function(){
+UserSchema.pre("save", async function () {
+  // console.log(this.modifiedPaths());
+  // console.log(this.isModified("name"));
 
-    console.log(this.modifiedPaths());
-    console.log(this.isModified('name'));
-    
-    
+  if (this.isModified("password")) {
     const salt = await bcrypt.genSalt(10);
 
     this.password = await bcrypt.hash(this.password, salt);
-
-})
+  }
+});
 
 // set up a compare method, the only way to compare is to take your current value, hash it, then compare those values
 UserSchema.methods.comparePassword = async function (candidatePassword) {
-    const isMatch = await bcrypt.compare(candidatePassword, this.password);
+  const isMatch = await bcrypt.compare(candidatePassword, this.password);
 
-    return isMatch;
-}
+  return isMatch;
+};
 
-
-module.exports = mongoose.model('User', UserSchema);
+module.exports = mongoose.model("User", UserSchema);
