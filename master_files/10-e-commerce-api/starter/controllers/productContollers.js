bd77df081d8e46b6c2b78466b8169c6c3d60815e
Caelan Roberge-Toll <caelanrt15@gmail.com>
2026-03-09 22:00:42 -0400

    adding the rest of the controllers for the product

--- master_files/10-e-commerce-api/starter/controllers/productContollers.js
+++ master_files/10-e-commerce-api/starter/controllers/productContollers.js
@@ -2,7 +2,7 @@ const Product = require("../models/Product");
 const CustomError = require("../errors/index");
 const { StatusCodes } = require("http-status-codes");
 
-// need to add async
+// if you want more than one admin, you should check to make sure that you are checking the userid so that its affiliated with the right admin/tenant
 const createProduct = async (req, res) => {
   req.body.user = req.user.userId;
 
@@ -11,20 +11,56 @@ const createProduct = async (req, res) => {
   res.status(StatusCodes.CREATED).json({ product });
 };
 
-const getAllProducts = (req, res) => {
-  res.send("getAllProducts");
+const getAllProducts = async (req, res) => {
+  const products = await Product.find({});
+
+  res.status(StatusCodes.OK).json({ products, count: products.length });
 };
 
-const getSingleProduct = (req, res) => {
-  res.send("getSingleProduct");
+const getSingleProduct = async (req, res) => {
+  const product = await Product.findOne({ _id: req.params.id });
+
+  if (!product) {
+    throw new CustomError.NotFoundError(
+      `No product found with ID ${req.params.id}`,
+    );
+  }
+
+  res.status(StatusCodes.OK).json({ product });
 };
 
-const updateProduct = (req, res) => {
-  res.send("updateProduct");
+const updateProduct = async (req, res) => {
+  const product = await Product.findOneAndUpdate(
+    { _id: req.params.id },
+    req.body,
+    { new: true, runValidators: true },
+  );
+
+  if (!product) {
+    throw new CustomError.NotFoundError(
+      `No product found with ID ${req.params.id}`,
+    );
+  }
+
+  res.status(StatusCodes.OK).json({ product });
 };
 
-const deleteProduct = (req, res) => {
-  res.send("deleteProduct");
+const deleteProduct = async (req, res) => {
+  const { id: productId } = req.params;
+
+  const product = await Product.findOne({ _id: productId });
+
+  if (!product) {
+    throw new CustomError.NotFoundError(
+      `No product found with ID ${req.params.id}`,
+    );
+  }
+
+  // .remove() has been deprecated so im gonna do some funny business and stick to the course, but just find one and delete here
+
+  const deleted = await Product.findOneAndDelete({ _id: productId });
+
+  res.status(StatusCodes.OK).json({ deleted });
 };
 
 const uploadImage = (req, res) => {
